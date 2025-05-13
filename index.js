69523ebc4d4b2d075ef9b6abed7c4e4b174a2d73
Kabileshwaran183 <Kabileshwaran183@gmail.com>
2025-05-13 09:24:17 +0530

    1st express using

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
+const port = 3000;
 
-app.listen(3000 , () => {
-    console.log('Server is running on port 3000');
+app.listen(port , () => {
+    console.log(`Server is running on port 3000 - http://localhost:${port}`);
 })
 
 app.get('/',(req,res)=>{
